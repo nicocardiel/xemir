e967e3fd042124a6f028431db56a1661967dbee3
Nicolás Cardiel <cardiel@ucm.es>
2017-07-05 09:32:06 +0200

    Include .gitignore and zzz_define_scripts.sh

---
+++ zzz_define_scripts.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# read CSUP<number> keywords from FITS header and display CSU bar locations
+alias EMIR_display_slitlet_arrangement=\
+'python ~/s/xemir/emir_slitlet/display_slitlet_arrangement.py'
+
+# display distortion map using mathematical expression
+alias EMIR_distorsion_emir_spec=\
+'python ~/s/xemir/emir_slitlet/distorsion_emir_spec.py'
+
+# fit boundaries
+alias EMIR_fit_boundaries=\
+'python ~/s/xemir/emir_slitlet/fit_boundaries.py'
+
+# overplot boundaries on image
+alias EMIR_overplot_bounddict=\
+'python ~/s/xemir/emir_slitlet/overplot_bounddict.py'
+
+# compute slitlet boundaries from continuum lamp exposures
+alias EMIR_slitlet_boundaries_from_continuum.py=\
+'python ~/s/xemir/emir_slitlet/slitlet_boundaries_from_continuum.py'
