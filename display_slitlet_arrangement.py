31c4d9a1ad1bf9af1397f42673b6c428b929bc98
Nicolás Cardiel <cardiel@ucm.es>
2017-07-21 12:42:16 +0200

    Update script with minor changes.

--- display_slitlet_arrangement.py
+++ display_slitlet_arrangement.py
@@ -9,6 +9,7 @@ import os.path
 from numina.array.display.pause_debugplot import pause_debugplot
 
 from emirdrp.core import EMIR_NBARS
+from numina.array.display.pause_debugplot import DEBUGPLOT_CODES
 
 
 def read_csup_from_header(image_header, debugplot=0):
@@ -30,25 +31,25 @@ def read_csup_from_header(image_header, debugplot=0):
 
     Returns
     -------
-    csu_bar_left : array, float
+    csu_bar_left : list of floats
         Location (mm) of the left bar for each slitlet.
-    csu_bar_right : array, float
+    csu_bar_right : list of floats
         Location (mm) of the right bar for each slitlet, using the
         same origin employed for csu_bar_left (which is not the
         value stored in the FITS keywords.
-    csu_bar_slit_center : array, float
+    csu_bar_slit_center : list of floats
         Middle point (mm) in between the two bars defining a slitlet.
-    csu_bar_slit_width : array, float
+    csu_bar_slit_width : list of floats
         Slitlet width (mm), computed as the distance between the two
         bars defining the slitlet.
 
     """
 
     # declare arrays to store CSU bar configuration
-    csu_bar_left = np.zeros(EMIR_NBARS)
-    csu_bar_right = np.zeros(EMIR_NBARS)
-    csu_bar_slit_center = np.zeros(EMIR_NBARS)
-    csu_bar_slit_width = np.zeros(EMIR_NBARS)
+    csu_bar_left = []
+    csu_bar_right = []
+    csu_bar_slit_center = []
+    csu_bar_slit_width = []
 
     # loop to read all the CSUP keywords
     if debugplot >= 10:
@@ -59,18 +60,17 @@ def read_csup_from_header(image_header, debugplot=0):
         ibar = i + 1
         keyword = 'CSUP' + str(ibar)
         if keyword in image_header:
-            csu_bar_left[i] = image_header[keyword]
+            csu_bar_left.append(image_header[keyword])
         else:
             raise ValueError("Expected keyword " + keyword + " not found!")
         keyword = 'CSUP' + str(ibar + EMIR_NBARS)
         if keyword in image_header:
-            csu_bar_right[i] = image_header[keyword]
             # set the same origin as the one employed for csu_bar_left
-            csu_bar_right[i] = 341.5 - csu_bar_right[i]
+            csu_bar_right.append(341.5 - image_header[keyword])
         else:
             raise ValueError("Expected keyword " + keyword + " not found!")
-        csu_bar_slit_center[i] = (csu_bar_left[i] + csu_bar_right[i])/2
-        csu_bar_slit_width[i] = csu_bar_right[i] - csu_bar_left[i]
+        csu_bar_slit_center.append((csu_bar_left[i] + csu_bar_right[i])/2)
+        csu_bar_slit_width.append(csu_bar_right[i] - csu_bar_left[i])
         if debugplot >= 10:
             print("{0:4d} {1:8.3f} {2:8.3f} {3:8.3f} {4:7.3f}".format(
                 ibar, csu_bar_left[i], csu_bar_right[i],
@@ -104,13 +104,13 @@ def read_csup_from_header(image_header, debugplot=0):
            csu_bar_slit_width
 
 
-def display_slitlet_arrangement(filename, bbox=None, debugplot=0):
+def display_slitlet_arrangement(file, bbox=None, debugplot=0):
     """Display slitlet arrangment from CSUP keywords in FITS header.
 
     Parameters
     ----------
-    filename : string
-        FITS file name.
+    file : file object
+        FITS file object.
     bbox : tuple of 4 floats
         If not None, values for xmin, xmax, ymin and ymax.
     debugplot : int
@@ -140,7 +140,7 @@ def display_slitlet_arrangement(filename, bbox=None, debugplot=0):
     """
 
     # read input FITS file
-    hdulist = fits.open(filename)
+    hdulist = fits.open(file.name)
     image_header = hdulist[0].header
     image2d = hdulist[0].data
     hdulist.close()
@@ -182,7 +182,7 @@ def display_slitlet_arrangement(filename, bbox=None, debugplot=0):
                                            csu_bar_slit_width[i], 1.0))
             ax.plot([0., csu_bar_left[i]], [ibar, ibar], 'o-')
             ax.plot([csu_bar_right[i], 341.5], [ibar, ibar], 'o-')
-        plt.title("File: " + filename + "\ngrism=" + grism +
+        plt.title("File: " + file.name + "\ngrism=" + grism +
                   ", filter=" + spfilter + ", rotang=" + str(round(rotang,2)))
         pause_debugplot(debugplot, pltshow=True)
 
@@ -196,49 +196,47 @@ def main(args=None):
     # parse command-line options
     parser = argparse.ArgumentParser(prog='display_slitlet_arrangement')
     parser.add_argument("filename",
-                        help="FITS file or txt file with list of FITS files")
+                        help="FITS files (wildcards accepted) or single TXT "
+                             "file with list of FITS files",
+                        type=argparse.FileType('r'),
+                        nargs='+')
     parser.add_argument("--bbox",
-                        help="bounding box tuple xmin,xmax,ymin,ymax")
+                        help="bounding box tuple xmin,xmax,ymin,"
+                             "ymax indicating plot limits")
     parser.add_argument("--debugplot",
-                        help="Integer indicating plotting/debugging" +
+                        help="Integer indicating plotting & debugging options"
                              " (default=12)",
                         default=12, type=int,
-                        choices=[0, 1, 2, 10, 11, 12, 21, 22])
+                        choices=DEBUGPLOT_CODES)
     args = parser.parse_args(args)
-
-    # check for input file
-    filename = args.filename
-    if not os.path.isfile(filename):
-        raise ValueError("File " + filename + " not found!")
 
     # read bounding box
     if args.bbox is None:
         bbox = None
     else:
-        tmp_bbox = args.bbox.split(",")
-        xmin = int(tmp_bbox[0])
-        xmax = int(tmp_bbox[1])
-        ymin = int(tmp_bbox[2])
-        ymax = int(tmp_bbox[3])
+        str_bbox = args.bbox.split(",")
+        xmin, xmax, ymin, ymax = [int(str_bbox[i]) for i in range(4)]
         bbox = xmin, xmax, ymin, ymax
 
-    # if input file is a txt file, assume it is a list of FITS files
-    if filename[-4:] == ".txt":
-        with open(filename) as f:
-            file_content = f.read().splitlines()
-        list_fits_files = []
-        for line in file_content:
-            if len(line) > 0:
-                if line[0] != '#':
-                    tmpfile = line.split()[0]
-                    if not os.path.isfile(tmpfile):
-                        raise ValueError("File " + tmpfile + " not found!")
-                    list_fits_files.append(tmpfile)
+    list_fits_file_objects = []
+    # if input file is a single txt file, assume it is a list of FITS files
+    if len(args.filename) == 1:
+        if args.filename[0].name[-4:] == ".txt":
+            file_content = args.filename[0].read().splitlines()
+            for line in file_content:
+                if len(line) > 0:
+                    if line[0] != '#':
+                        tmpfile = line.split()[0]
+                        if not os.path.isfile(tmpfile):
+                            raise ValueError("File " + tmpfile + " not found!")
+                        list_fits_file_objects.append(open(tmpfile, 'r'))
+        else:
+            list_fits_file_objects = [args.filename[0]]
     else:
-        list_fits_files = [filename]
+        list_fits_file_objects = args.filename
 
     # total number of files to be examined
-    nfiles = len(list_fits_files)
+    nfiles = len(list_fits_file_objects)
 
     # declare arrays to store CSU values
     csu_bar_left = np.zeros((nfiles, EMIR_NBARS))
@@ -247,8 +245,8 @@ def main(args=None):
     csu_bar_slit_width = np.zeros((nfiles, EMIR_NBARS))
 
     # display CSU bar arrangement
-    for ifile, file in enumerate(list_fits_files):
-        print("\nFile " + str(ifile+1) + "/" + str(nfiles) + ": " + file)
+    for ifile, file in enumerate(list_fits_file_objects):
+        print("\nFile " + str(ifile+1) + "/" + str(nfiles) + ": " + file.name)
         csu_bar_left[ifile, :], csu_bar_right[ifile, :], \
         csu_bar_slit_center[ifile, :], csu_bar_slit_width[ifile, :] = \
             display_slitlet_arrangement(file, bbox=bbox,
@@ -291,7 +289,7 @@ def main(args=None):
             print("Total number of files examined:", nfiles)
 
     # stop program execution
-    if len(list_fits_files) > 1:
+    if len(list_fits_file_objects) > 1:
         pause_debugplot(12, optional_prompt="Press RETURN to STOP")
 
 
