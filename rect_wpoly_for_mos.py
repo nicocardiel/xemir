9ebd2af5f90a762253fc267392e56e9b418c4943
Nicolás Cardiel <cardiel@ucm.es>
2017-11-06 16:48:44 +0100

    Interpolate and save (in output JSON file) the coefficients corresponding to the rectification transformations (ttd_aij, ttd_bij, tti_aij, tti_bij).

--- rect_wpoly_for_mos.py
+++ rect_wpoly_for_mos.py
@@ -162,10 +162,10 @@ def main(args=None):
         print('order_no_duplicates:', order_no_duplicates)
         raise ValueError('tdd_order is not constant!')
     order = int(order_no_duplicates[0])
-    ncoef = ncoef_fmap(order)
+    ncoef_rect = ncoef_fmap(order)
     if abs(args.debugplot) >= 10:
-        print('>>> ttd_order:', order)
-        print('>>> ncoef....:', ncoef)
+        print('>>> ttd_order....:', order)
+        print('>>> ncoef_rect...:', ncoef_rect)
 
     # check that polynomial degree in frontiers and spectrails are the same
     poldeg_check_list = []
@@ -183,42 +183,67 @@ def main(args=None):
             poldeg_check_list.append(len(tmppoly))
             tmppoly = tmpdict[cslitlet]['spectrail']['poly_coef_upper']
             poldeg_check_list.append(len(tmppoly))
-        # remove duplicates in list
-        poldeg_no_duplicates = list(set(poldeg_check_list))
-        if len(poldeg_no_duplicates) != 1:
-            print('poldeg_no_duplicates:', order_no_duplicates)
-            raise ValueError('poldeg is not constant in frontiers and '
-                             'spectrails!')
-        poldeg = int(poldeg_no_duplicates[0])
-        if abs(args.debugplot) >= 10:
-            print('>>> poldeg...:', poldeg)
+    # remove duplicates in list
+    poldeg_no_duplicates = list(set(poldeg_check_list))
+    if len(poldeg_no_duplicates) != 1:
+        print('poldeg_no_duplicates:', order_no_duplicates)
+        raise ValueError('poldeg is not constant in frontiers and '
+                         'spectrails!')
+    poldeg = int(poldeg_no_duplicates[0])
+    if abs(args.debugplot) >= 10:
+        print('>>> poldeg.......:', poldeg)
 
     # ---
 
     # Interpolate rectification polynomial coefficients
 
+    # note: when aij and bij have not been computed, we use the modeled
+    # version aij_modeled and bij_modeled
     for islitlet in range(islitlet_min, islitlet_max + 1):
+        if abs(args.debugplot) == 0:
+            if islitlet % 10 == 0:
+                cout = str(islitlet // 10)
+            else:
+                cout = '.'
+            sys.stdout.write(cout)
+            if islitlet == islitlet_max:
+                sys.stdout.write('\n')
+            sys.stdout.flush()
+
         cslitlet = 'slitlet' + str(islitlet).zfill(2)
-        for icoef in range(ncoef):
-            list_csu_bar_slit_center = []
-            list_ttd_aij = []
-            for ifile in range(nfiles):
-                tmpdict = list_json_longslits[ifile]['contents'][cslitlet]
-                csu_bar_slit_center = tmpdict['csu_bar_slit_center']
-                ttd_aij = tmpdict['ttd_aij']
-                if ttd_aij is not None:
+        outdict['contents'][cslitlet] = {}
+        for keycoef in ['ttd_aij', 'ttd_bij', 'tti_aij', 'tti_bij']:
+            list_cc_rect = []
+            for icoef in range(ncoef_rect):
+                list_csu_bar_slit_center = []
+                list_cij = []
+                for ifile in range(nfiles):
+                    tmpdict = list_json_longslits[ifile]['contents'][cslitlet]
+                    csu_bar_slit_center = tmpdict['csu_bar_slit_center']
                     list_csu_bar_slit_center.append(csu_bar_slit_center)
-                    list_ttd_aij.append(ttd_aij[icoef])
-            poly, yres, reject = polfit_residuals_with_sigma_rejection(
-                x=np.array(list_csu_bar_slit_center),
-                y=np.array(list_ttd_aij),
-                deg=2,
-                times_sigma_reject=5,
-                xlabel='csu_bar_slit_center',
-                ylabel='tti_aij[' + str(icoef) + ']',
-                title=cslitlet,
-                debugplot=0
-            )
+                    cij = tmpdict[keycoef]
+                    if cij is not None:
+                        list_cij.append(cij[icoef])
+                    else:
+                        cij_modeled = tmpdict[keycoef + '_modeled']
+                        if cij_modeled is None:
+                            raise ValueError("Unexpected cij_modeled=None!")
+                        else:
+                            list_cij.append(cij_modeled[icoef])
+                poly, yres, reject = polfit_residuals_with_sigma_rejection(
+                    x=np.array(list_csu_bar_slit_center),
+                    y=np.array(list_cij),
+                    deg=2,
+                    times_sigma_reject=5,
+                    xlabel='csu_bar_slit_center',
+                    ylabel=keycoef + '[' + str(icoef) + ']',
+                    title=cslitlet,
+                    debugplot=args.debugplot
+                )
+                list_cc_rect.append(poly.coef.tolist())
+            outdict['contents'][cslitlet][keycoef] = {}
+            for k, tmpcoef in enumerate(list_cc_rect):
+                outdict['contents'][cslitlet][keycoef][k] = tmpcoef
 
     # ---
 
