6387f3bb592b67e6f088b6f67842c5445e3843e2
Nicolás Cardiel <cardiel@ucm.es>
2017-11-06 17:54:30 +0100

    Define alias for new script.

--- zzz_define_scripts.sh
+++ zzz_define_scripts.sh
@@ -30,3 +30,7 @@ alias EMIR_wpoly_from_longslit=\
 alias EMIR_rect_wpoly_for_mos=\
 'python ~/s/xemir/rect_wpoly_for_mos.py'
 
+# apply rectification and wavelength calibration transformations
+alias EMIR_apply_rect_wpoly=\
+'python ~/s/xemir/apply_rect_wpoly.py'
+
